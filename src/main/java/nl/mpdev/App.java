467c87ea9ced43e0ee434f80e658b8f2ed91ce4a
mjerta <maarten.postma1000@gmail.com>
2025-01-17 16:24:23 +0100

    Add in the Jframe

--- src/main/java/nl/mpdev/App.java
+++ src/main/java/nl/mpdev/App.java
@@ -1,7 +1,15 @@
 package nl.mpdev;
 
+import javax.swing.*;
+
 public class App {
   public static void main(String[] args) {
-
+    int w = 640;
+    int h = 480;
+    JFrame f = new JFrame();
+    f.setSize(w, h);
+    f.setTitle("Drawning in Java");
+    f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    f.setVisible(true);
   }
 }
