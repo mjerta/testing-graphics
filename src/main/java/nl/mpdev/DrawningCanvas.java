c7fe12015e2d3f2ee88b848d90922d679718416d
mjerta <maarten.postma1000@gmail.com>
2025-01-18 11:39:07 +0100

    Add antialiasing

--- src/main/java/nl/mpdev/DrawningCanvas.java
+++ src/main/java/nl/mpdev/DrawningCanvas.java
@@ -19,6 +19,11 @@ public class DrawningCanvas extends JComponent {
     // casten from Graphics to Graphics2D
     Graphics2D g2d = (Graphics2D) g;
 
+    RenderingHints rh = new RenderingHints(
+      RenderingHints.KEY_ANTIALIASING,
+      RenderingHints.VALUE_ANTIALIAS_ON
+    );
+    g2d.setRenderingHints(rh);
 
 
     Rectangle2D.Double r = new Rectangle2D.Double(0, 0, width, height);
