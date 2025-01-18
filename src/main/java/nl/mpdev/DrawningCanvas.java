2038e6a0b25991b383090f55dc4a17bcf95458d8
mjerta <maarten.postma1000@gmail.com>
2025-01-18 11:26:45 +0100

    Drawning a line

--- src/main/java/nl/mpdev/DrawningCanvas.java
+++ src/main/java/nl/mpdev/DrawningCanvas.java
@@ -3,9 +3,10 @@ package nl.mpdev;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.geom.Ellipse2D;
+import java.awt.geom.Line2D;
 import java.awt.geom.Rectangle2D;
 
-public class DrawningCanvas  extends JComponent {
+public class DrawningCanvas extends JComponent {
   private int width;
   private int height;
 
@@ -17,11 +18,19 @@ public class DrawningCanvas  extends JComponent {
   protected void paintComponent(Graphics g) {
     // casten from Graphics to Graphics2D
     Graphics2D g2d = (Graphics2D) g;
-    Rectangle2D.Double r = new Rectangle2D.Double(0,0 ,width,height);
-    g2d.setColor(new Color(100,149,237));
+
+
+
+    Rectangle2D.Double r = new Rectangle2D.Double(0, 0, width, height);
+    g2d.setColor(new Color(100, 149, 237));
     g2d.fill(r);
 
-    Ellipse2D.Double e = new Ellipse2D.Double(10,10 ,20,20);
+    Line2D.Double l = new Line2D.Double(100, 250, 300, 75);
+    g2d.setColor(Color.BLACK);
+    // For lines we used draw
+    g2d.draw(l);
+
+    Ellipse2D.Double e = new Ellipse2D.Double(200, 75, 100, 100);
     g2d.setColor(Color.RED);
     g2d.fill(e);
 
