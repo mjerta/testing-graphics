34814a1c4e8d384e4e905c3a698ad3d0238acf8d
mjerta <maarten.postma1000@gmail.com>
2025-01-23 16:34:10 +0100

    JFrame implements keylistener

--- src/main/java/nl/mpdev/App.java
+++ src/main/java/nl/mpdev/App.java
@@ -5,6 +5,8 @@ import java.awt.*;
 
 public class App {
   public static void main(String[] args) {
+
     JFrame f = new CustomFrame("Drawning in java", new CustomPanel(640,480,20));
+
   }
 }
--- src/main/java/nl/mpdev/CustomFrame.java
+++ src/main/java/nl/mpdev/CustomFrame.java
@@ -1,16 +1,34 @@
 package nl.mpdev;
 
 import javax.swing.*;
-import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class CustomFrame extends JFrame {
+public class CustomFrame extends JFrame implements KeyListener {
 
   public CustomFrame(String title, CustomPanel customPanel) {
     this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    this.setVisible(true);
     this.setTitle(title);
-    this.setLayout(null);
+//    this.setLayout(null);
     this.add(customPanel);
+    this.addKeyListener(this);
     this.pack();
+    this.setVisible(true);
+  }
+
+  @Override
+  public void keyTyped(KeyEvent e) {
+
+  }
+
+  @Override
+  public void keyPressed(KeyEvent e) {
+
+  }
+
+  @Override
+  public void keyReleased(KeyEvent e) {
+    System.out.println("The following character is pressed: " + e.getKeyChar());
+
   }
 }
