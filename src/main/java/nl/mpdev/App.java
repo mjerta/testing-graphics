f65403fd15aeb04c75d749c538f5bf3f665acb40
mjerta <maarten.postma1000@gmail.com>
2025-01-23 17:38:33 +0100

    Getting the first keystroke animation in

--- src/main/java/nl/mpdev/App.java
+++ src/main/java/nl/mpdev/App.java
@@ -1,13 +1,10 @@
 package nl.mpdev;
 
 import javax.swing.*;
-import java.awt.*;
 
 public class App {
   public static void main(String[] args) {
 
     JFrame f = new CustomFrame("Drawning in java", new CustomPanel(640,480,20));
-    System.out.println("test");
-
   }
 }
--- src/main/java/nl/mpdev/CustomFrame.java
+++ src/main/java/nl/mpdev/CustomFrame.java
@@ -1,33 +1,49 @@
 package nl.mpdev;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 public class CustomFrame extends JFrame implements KeyListener {
+
+  private final JLabel label;
 
   public CustomFrame(String title, CustomPanel customPanel) {
     this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     this.setTitle(title);
     this.setLayout(null);
 //    this.add(customPanel);
-    this.addKeyListener(this);
+    this.setPreferredSize(new Dimension(500, 500));
     this.pack();
+    this.setLocationRelativeTo(null);
+    this.addKeyListener(this);
+
+    label = new JLabel();
+    label.setBounds(0, 0, 100, 100);
+    label.setBackground(Color.RED);
+    label.setOpaque(true);
+    this.add(label);
     this.setVisible(true);
   }
 
   @Override
   public void keyTyped(KeyEvent e) {
+
+    switch (e.getKeyChar()) {
+      case 'a':
+        label.setLocation(label.getX() - 1, label.getY());
+
+    }
   }
 
   @Override
   public void keyPressed(KeyEvent e) {
-    System.out.println("Key code is being pressed: " + e.getKeyCode());
 
   }
 
   @Override
   public void keyReleased(KeyEvent e) {
-//    System.out.println("Key Released: Code=" + e.getKeyCode() + ", Char=" + e.getKeyChar());
+    System.out.println("Key Released: Code=" + e.getKeyCode() + ", Char=" + e.getKeyChar());
   }
 }
